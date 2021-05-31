fe2cee55737f11df65013bb109fd0d3a08273716
mohnalkhateeb <mhmmd.alkateeb@gmail.com>
2021-05-31 12:43:16 +0300

    addphoto

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 'use strict'
-let Ball = function(game,quntity,name,random)
+let imgArr = ['basket.png','bessball.jpg','football.jpg','golf.jpg','regpy.jpg','tenes.jpg','volipall.jpg']
+let Ball = function(source,game,quntity,name,random)
 {
+    this.source=source;
     this.game=game
     this.quntity=quntity
     this.name=name
@@ -19,13 +21,21 @@ function addball(event)
   let quntity = event.target.qun.value
   let name =event.target.name.value
   let random = Math.ceil(Math.random() * 10)
-  new Ball (game,quntity,name,random)
+  let source;
+  for (let i=0;i<imgArr.length;i++)
+  {
+      if (game == imgArr[i].split('.')[0])
+      {source='img/'+imgArr[i];}
+  }
+  new Ball (source,game,quntity,name,random)
   render()
   savetoLocalstorge();
-  
+  console.log(Ball.Balls)
+  console.log(source)
+
 
 }
-console.log(Ball.Balls)
+
 function savetoLocalstorge()
 {
     let info = JSON.stringify(Ball.Balls)
@@ -45,6 +55,11 @@ function render()
    for(let i=0;i<Ball.Balls.length;i++) 
    {
        let trEl = document.createElement('tr')
+       let td0El = document.createElement('td')
+       let imgEl = document.createElement('img')
+       imgEl.setAttribute('src',Ball.Balls[i].source)
+       td0El.appendChild(imgEl)
+       trEl.appendChild(td0El)
        let td1El = document.createElement('td')
        td1El.textContent = Ball.Balls[i].name;
        trEl.appendChild(td1El);
